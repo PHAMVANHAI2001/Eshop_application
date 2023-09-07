ab66dc28c5bc960a46fb678b187459f519cfb9f8
Hải Phạm <71239348+PHAMVANHAI2001@users.noreply.github.com>
2023-09-07 15:30:52 +0700

    update page product detail

--- Eshop_admin/src/main/java/com/eshop/app/EshopApplication.java
+++ Eshop_admin/src/main/java/com/eshop/app/EshopApplication.java
@@ -2,9 +2,17 @@ package com.eshop.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.quartz.QuartzAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@SpringBootApplication(exclude = {
+  SecurityAutoConfiguration.class,
+  JpaRepositoriesAutoConfiguration.class,
+  DataSourceAutoConfiguration.class,
+  QuartzAutoConfiguration.class
+})
 public class EshopApplication {
 
 	public static void main(String[] args) {
--- Eshop_admin/src/main/java/com/eshop/app/services/UserService.java
+++
@@ -1,7 +0,0 @@
-package com.eshop.app.services;
-
-import com.eshop.app.entities.User;
-
-public interface UserService {
-    User findByUsername(String username);
-}
